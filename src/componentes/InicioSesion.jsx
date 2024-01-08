f27bb221f6cfb40bd1e72b26192dffdb35523b79
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2024-01-08 14:12:42 +0100

    Signup and Signin forms

--- src/componentes/InicioSesion.jsx
+++ src/componentes/InicioSesion.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
+import Boton from "../elementos/Boton";
+import BtnRegresar from "../elementos/BtnRegresar";
+import {
+  Header,
+  Titulo,
+  ContenedorBotones,
+  ContenedorHeader,
+} from "../elementos/Header";
+import {ContenedorBoton,ContenedorFiltros,Formulario,Input,InputGrande} from '../elementos/ElementosDeFormulario'
+import { Form } from "react-router-dom";
+import {ReactComponent as SvgLogin} from "../imagenes/login.svg"
+import styled from "styled-components";
 
-export default function InicioSesion() {
+const Svg =styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem;
+  margin-bottom: 1.25rem;
+`;
+export default function RegistroUsuarios() {
   return (
-    <div>InicioSesion</div>
-  )
+    <>
+      <Helmet>
+        <title>Inicio de Sesión</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Iniciar sesión</Titulo>
+          <div>
+            <Boton to="/crear-cuenta">Registrarse</Boton>
+          </div>
+        </ContenedorHeader>
+      </Header>
+      <Formulario>
+        <Svg/>
+       <Input type="email" name="email" placeholder="Correo Electrónico" />
+       <Input type="password" name="password" placeholder="Contraseñas" />
+       <ContenedorBoton>
+        <Boton as="button" primario type="submit" >Iniciar Sesión</Boton>
+       </ContenedorBoton>
+      </Formulario>
+    </>
+  );
 }
--- src/componentes/RegistroUsuarios.jsx
+++ src/componentes/RegistroUsuarios.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Boton from "../elementos/Boton";
 import BtnRegresar from "../elementos/BtnRegresar";
-
 import {
   Header,
   Titulo,
   ContenedorBotones,
   ContenedorHeader,
 } from "../elementos/Header";
+import {ContenedorBoton,ContenedorFiltros,Formulario,Input,InputGrande} from '../elementos/ElementosDeFormulario'
+import { Form } from "react-router-dom";
+import {ReactComponent as SvgLogin} from "../imagenes/registro.svg"
+import styled from "styled-components";
 
+const Svg =styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem;
+  margin-bottom: 1.25rem;
+`;
 export default function RegistroUsuarios() {
   return (
     <>
@@ -25,6 +33,15 @@ export default function RegistroUsuarios() {
           </div>
         </ContenedorHeader>
       </Header>
+      <Formulario>
+        <Svg/>
+       <Input type="email" name="email" placeholder="Correo Electrónico" />
+       <Input type="password" name="password" placeholder="Contraseñas" />
+       <Input type="password" name="password" placeholder="Repetir Contraseña" />
+       <ContenedorBoton>
+        <Boton as="button" primario type="submit" >Crear Cuenta</Boton>
+       </ContenedorBoton>
+      </Formulario>
     </>
   );
 }
